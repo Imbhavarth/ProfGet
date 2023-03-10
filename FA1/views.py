d8e6385c43978465e63a7619d46d49e3bdf1d431
KathanVPrajapati <127566325+KathanVPrajapati@users.noreply.github.com>
2023-03-11 04:23:18 +0530

    Add files via upload

---
+++ FA1/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'index.html')
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['uname']
+        password = request.POST['psw']
+    return render(request,'authentification/login.html')
